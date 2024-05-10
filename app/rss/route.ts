3fa7672322577f46310ffbe32ff6dfbce710be68
Vincent Trivett <trivett@gmail.com>
2024-05-10 07:22:10 -0400

    rss

--- app/rss/route.ts
+++ app/rss/route.ts
@@ -1,28 +1,28 @@
 import { baseUrl } from "app/sitemap";
-// import { getBlogPosts } from "app/blog/utils";
+import { getBlogPosts } from "app/blog/utils";
 
 export async function GET() {
-  // let allBlogs = await getBlogPosts();
+  let allBlogs = await getBlogPosts();
 
-  // const itemsXml = allBlogs
-  //   .sort((a, b) => {
-  //     if (new Date(a.metadata.publishedAt) > new Date(b.metadata.publishedAt)) {
-  //       return -1;
-  //     }
-  //     return 1;
-  //   })
-  //   .map(
-  //     (post) =>
-  //       `<item>
-  //         <title>${post.metadata.title}</title>
-  //         <link>${baseUrl}/blog/${post.slug}</link>
-  //         <description>${post.metadata.summary || ""}</description>
-  //         <pubDate>${new Date(
-  //           post.metadata.publishedAt
-  //         ).toUTCString()}</pubDate>
-  //       </item>`
-  //   )
-  //   .join("\n");
+  const itemsXml = allBlogs
+    .sort((a, b) => {
+      if (new Date(a.metadata.publishedAt) > new Date(b.metadata.publishedAt)) {
+        return -1;
+      }
+      return 1;
+    })
+    .map(
+      (post) =>
+        `<item>
+          <title>${post.metadata.title}</title>
+          <link>${baseUrl}/blog/${post.slug}</link>
+          <description>${post.metadata.summary || ""}</description>
+          <pubDate>${new Date(
+            post.metadata.publishedAt
+          ).toUTCString()}</pubDate>
+        </item>`
+    )
+    .join("\n");
 
   const rssFeed = `<?xml version="1.0" encoding="UTF-8" ?>
   <rss version="2.0">
@@ -31,6 +31,7 @@ export async function GET() {
         <link>${baseUrl}</link>
         <description>This is my portfolio RSS feed</description>
     </channel>
+    ${itemsXml}
   </rss>`;
 
   return new Response(rssFeed, {
