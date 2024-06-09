8ff11e2cc641a08a8f4ba0011a45b595a660bf55
Vincent Trivett <trivett@gmail.com>
2024-06-09 18:58:28 -0400

    remove global footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,6 @@ import { GeistMono } from "geist/font/mono";
 import { Navbar } from "./components/nav";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
-import Footer from "./components/footer";
 import { baseUrl } from "./sitemap";
 
 export const metadata: Metadata = {
@@ -46,17 +45,16 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      // className={cx(
-      //   "text-black bg-white dark:text-white dark:bg-black",
-      //   GeistSans.variable,
-      //   GeistMono.variable
-      // )}
+      className={cx(
+        "text-black bg-white dark:text-white dark:bg-black",
+        GeistSans.variable,
+        GeistMono.variable
+      )}
     >
       <body className="antialiased max-w-xl mx-4 mt-8 lg:mx-auto">
         <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
           <Navbar />
           {children}
-          <Footer />
           <Analytics />
           <SpeedInsights />
         </main>
