3cb2140c448d36bed7d6f6b65c5e5954a113ac29
Vincent Trivett <trivett@gmail.com>
2024-06-09 18:56:38 -0400

    make nav items a component

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -15,32 +15,31 @@ function ArrowIcon() {
   );
 }
 
+const ContactLineItem = ({ name, url }) => {
+  return (
+    <li>
+      <a
+        className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
+        rel="noopener noreferrer"
+        target="_blank"
+        href={url}
+      >
+        <ArrowIcon />
+        <p className="ml-2 h-7">{name}</p>
+      </a>
+    </li>
+  );
+};
+
 export default function Footer() {
   return (
     <footer className="mb-16 print:hidden">
-      <ul className="font-sm mt-8 flex flex-col space-x-0 space-y-2 text-neutral-600 md:flex-row md:space-x-4 md:space-y-0 dark:text-neutral-300">
-        <li>
-          <a
-            className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
-            rel="noopener noreferrer"
-            target="_blank"
-            href="https://github.com/trivett"
-          >
-            <ArrowIcon />
-            <p className="ml-2 h-7">Github</p>
-          </a>
-        </li>
-        <li>
-          <a
-            className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
-            rel="noopener noreferrer"
-            target="_blank"
-            href="https://www.linkedin.com/in/vtrivett/"
-          >
-            <ArrowIcon />
-            <p className="ml-2 h-7">LinkedIn</p>
-          </a>
-        </li>
+      <ul className="font-sm mt-8 flex flex-col space-x-0 space-y-2">
+        <ContactLineItem name="Github" url="https://github.com/trivett" />
+        <ContactLineItem
+          name="LinkedIn"
+          url="https://www.linkedin.com/in/vtrivett/"
+        />
       </ul>
     </footer>
   );
