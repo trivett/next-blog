d6afe7dcc5838ce347303fbd86fe28d2bea5f62b
Vincent Trivett <trivett@gmail.com>
2024-06-09 19:14:00 -0400

    remove default font and add fontawesome

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,5 @@
 import "./global.css";
 import type { Metadata } from "next";
-import { GeistSans } from "geist/font/sans";
-import { GeistMono } from "geist/font/mono";
 import { Navbar } from "./components/nav";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
@@ -34,8 +32,6 @@ export const metadata: Metadata = {
     },
   },
 };
-
-const cx = (...classes) => classes.filter(Boolean).join(" ");
 
 export default function RootLayout({
   children,
